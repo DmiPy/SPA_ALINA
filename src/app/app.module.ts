40ffe166041c90947d32d8214a4d30a640a55eff
Dmitriy Kozyrev <dmitry.kozyrev4@gmail.com>
2026-02-06 21:42:24 +0100

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImpressumComponent } from './impressum/impressum.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrivacyPolicyComponent,
-    ImpressumComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { ImpressumComponent } from './impressum/impressum.component';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    // { path: '', component: HomeComponent },
-    // { path: 'privacy-policy', component: PrivacyPolicyComponent},
-    // { path: 'impressum', component: ImpressumComponent},
+
 ];
